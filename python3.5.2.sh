8c50c74c694bbfef0a4a180286cc4f6ff826983b
YuSangHuck <g01099387636@gmail.com>
2017-04-22 23:57:58 +0000

    make -j

--- python3.5.2.sh
+++ python3.5.2.sh
@@ -8,7 +8,7 @@ apt-get update; apt-get install -y --no-install-recommends wget gcc g++ make
 cd ~; wget -q https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tgz; wget -q https://repo.continuum.io/miniconda/Miniconda3-4.2.11-Linux-x86_64.sh
 
 # install Python3.5.2 & Anaconda3-4.2.0
-cd ~; tar -xzf Python-3.5.2.tgz; cd Python-3.5.2; ./configure; make; make install
+cd ~; tar -xzf Python-3.5.2.tgz; cd Python-3.5.2; ./configure; make -j"$(nproc)"; make install
 cd ~; bash Miniconda3-4.2.11-Linux-x86_64.sh -b
 
 # move libpython3.5m.so(Anaconda) -> python3.5.2
