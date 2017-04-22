bf24e76536c3d37863c58585f194a495d21a946b
YuSangHuck <g01099387636@gmail.com>
2017-04-22 15:43:01 +0000

    Anaconda3 -> Miniconda3

--- cntk.sh
+++ cntk.sh
@@ -13,3 +13,5 @@ cd ~; tar -xzf openmpi-1.10.4.tar.gz; cd openmpi-1.10.4; ./configure --prefix=/u
 # install cntk2.0-beta12
 cd ~; tar -xzf CNTK-2-0-beta12-0-linux-64bit-CPU-Only.tar.gz; pip3 install ./cntk/cntk/python/cntk-2.0.beta12.0-cp35-cp35m-linux_x86_64.whl Pillow;
 
+# clean
+#rm -rf ~/CNTK* ~/openmpi* /var/lib/apt/lists/*; apt-get autoremove
--- python3.5.2.sh
+++ python3.5.2.sh
@@ -5,15 +5,15 @@
 apt-get update; apt-get install -y --no-install-recommends wget gcc g++ make
 
 # download Python3.5.2 & Anaconda3-4.2.0
-cd ~; wget -q https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tgz; wget -q https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh
+cd ~; wget -q https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tgz; wget -q https://repo.continuum.io/miniconda/Miniconda3-4.2.11-Linux-x86_64.sh
 
 # install Python3.5.2 & Anaconda3-4.2.0
 cd ~; tar -xzf Python-3.5.2.tgz; cd Python-3.5.2; ./configure; make; make install
-cd ~; bash Anaconda3-4.2.0-Linux-x86_64.sh
+cd ~; bash Miniconda3-4.2.0-Linux-x86_64.sh -b
 
 # move libpython3.5m.so(Anaconda) -> python3.5.2
-mv ~/anaconda3/lib/libpython3.5m.so.1.0 /usr/lib/x86_64-linux-gnu/
+mv ~/miniconda3/lib/libpython3.5m.so.1.0 /usr/lib/x86_64-linux-gnu/
 ln -s /usr/lib/x86_64-linux-gnu/libpython3.5m.so.1.0 /usr/lib/x86_64-linux-gnu/libpython3.5m.so
 
 # clean
-rm -rf ~/Python* ~/Anaconda* ~/anaconda
+rm -rf ~/Python* ~/Miniconda* ~/miniconda
