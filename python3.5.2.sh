e3efafd060ebf7cc2bcb5ebcfb581fe00efdf7f1
YuSangHuck <g01099387636@gmail.com>
2017-04-22 17:06:27 +0000

    OK

--- python3.5.2.sh
+++ python3.5.2.sh
@@ -9,11 +9,11 @@ cd ~; wget -q https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tgz; wget -q 
 
 # install Python3.5.2 & Anaconda3-4.2.0
 cd ~; tar -xzf Python-3.5.2.tgz; cd Python-3.5.2; ./configure; make; make install
-cd ~; bash Miniconda3-4.2.0-Linux-x86_64.sh -b
+cd ~; bash Miniconda3-4.2.11-Linux-x86_64.sh -b
 
 # move libpython3.5m.so(Anaconda) -> python3.5.2
 mv ~/miniconda3/lib/libpython3.5m.so.1.0 /usr/lib/x86_64-linux-gnu/
 ln -s /usr/lib/x86_64-linux-gnu/libpython3.5m.so.1.0 /usr/lib/x86_64-linux-gnu/libpython3.5m.so
 
 # clean
-rm -rf ~/Python* ~/Miniconda* ~/miniconda
+rm -rf ~/Python* ~/Miniconda* ~/miniconda*
