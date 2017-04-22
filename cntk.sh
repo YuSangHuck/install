c09c53dfa0ca0c6cf4de17acf3042770d282cba2
YuSangHuck <g01099387636@gmail.com>
2017-04-22 21:39:43 +0900

    add cntk.sh file

---
+++ cntk.sh
@@ -0,0 +1,17 @@
+# !/bin/bash
+# install cntk2.0-beta12 at python3.5.2
+
+# install basic
+apt-get update; apt-get install –y —no-install-recommends ca-certificates wget gcc g++ make libjasper1 libjpeg8 libpng12-0 libgfortran3	vim git python3-pip libssl-dev libreadline6-dev zlibc zlib1g-dev
+
+# download open-mpi-1.10.4 & cntk2.0-beta12
+cd ~; wget –q https://www.open-mpi.org/software/ompi/v1.10/downloads/openmpi-1.10.4.tar.gz; wget –q https://cntk.ai/BinaryDrop/CNTK-2-0-beta12-0-linux-64bit-CPU-Only.tar.gz
+
+# install open-mpi-1.10.4
+cd ~; tar –xzf openmpi-1.10.4.tar.gz; cd openmpi-1.10.4; ./configure —prefix=/usr/local/mpi; make –j“$(nproc)” install; echo ’export PATH=/usr/local/mpi/bin:$PATH’ >> ~/.bashrc; echo ’export LD_LIBRARY_PATH=/usr/local/mpi/lib:$LD_LIBRARY_PATH’ >> ~/.bashrc
+
+# install cntk2.0-beta12
+cd ~; tar –xzf CNTK-2-0-beta12-0-linux-64bit-CPU-Only.tar.gz; pip3 install ./cntk/cntk/python/cntk-2.0.beta12.0-cp35-cp35m-linux_x86_64.whl Pillow;
+
+# clean
+cd ~; rm -rf *; apt-get –y autoremove; rm –rf /var/lib/apt/lists/*; rm –rf ~/*
