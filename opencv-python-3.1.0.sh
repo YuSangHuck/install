a74a5f0a805f944bcb15174a49e8b1faa8854a54
YuSangHuck <g01099387636@gmail.com>
2017-04-23 08:18:09 +0000

    OK

--- opencv-python-3.1.0.sh
+++ opencv-python-3.1.0.sh
@@ -6,16 +6,11 @@ apt-get update
 apt-get upgrade
 
 # install library
-apt-get install -y --no-install-commends wget \
-                ca-certificates build-essential \
-		cmake git pkg-donfig libjpeg8-dev \
-		libtiff5-dev libjasper-dev libpng12-dev \
-		libgtk-3-dev libavcodec-dev libavformat-dev \
-		libswscale-dev libv4l-dev libatlas-base-dev \
-		gfortran python3.5-dev
+apt-get install -y --no-install-commends wget ca-certificates build-essential cmake git pkg-donfig libjpeg8-dev libtiff5-dev libjasper-dev libpng12-dev libgtk-3-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libatlas-base-dev gfortran python3.5-dev make
 
 # install pip & numpy
-cd ~; wget -q https://bootstrap.pypa.io/get-pip.py; python3.5 get-pip.py
+#cd ~; wget -q https://bootstrap.pypa.io/get-pip.py; 
+python3.5 get-pip.py
 pip install numpy
 
 # cloning OpenCV-Python from Git
@@ -24,12 +19,7 @@ git clone https://github.com/opencv/opencv_contrib.git;
 
 # make Makefile
 cd opencv; mkdir build; cd build;
-cmake -D CMAKE_BUILD_TYPE=RELEASE \
-      -D CMAKE_INSTALL_PREFIX=/usr/local \
-	  -D INSTALL_C_EXAMPLES=OFF \
-	  -D INSTALL_PYTHON_EXAMPLES=ON \
-	  -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
-	  -D BUILD_EXAMPLES=ON ..
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D INSTALL_C_EXAMPLES=OFF -D INSTALL_PYTHON_EXAMPLES=ON -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules -D BUILD_EXAMPLES=ON ..
 
 # compile OpenCV-Python
 make -j"$(nproc)"
