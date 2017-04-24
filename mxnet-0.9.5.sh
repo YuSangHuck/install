dc30ed002236cba226c12c8cacb8f9418b94f81b
YuSangHcuk <g01099387636@gmail.com>
2017-04-24 15:15:07 +0000

    need to check

--- mxnet-0.9.5.sh
+++ mxnet-0.9.5.sh
@@ -3,13 +3,10 @@
 
 # install basic
 apt-get update
-apt-get install -y --no-install-recommends build-essential git python3.5-dev
+apt-get install -y --no-install-recommends build-essential git
 
 # install Blas library
 apt-get install -y --no-install-recommends libatlas-base-dev
-
-# install pip=3.5
-cd ~; wget https://bootstrap.pypa.io/get-pip.py; python3.5 get-pip.py
 
 # build mxnet
 git clone --recursive https://github.com/dmlc/mxnet; cd mxnet; make -j"$(nproc)"
