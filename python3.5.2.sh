57b1be0bc4c380c6b5f0c33735ff4621f9c57820
YuSangHuck <g01099387636@gmail.com>
2017-04-24 07:56:48 +0000

    OK

--- python3.5.2.sh
+++ python3.5.2.sh
@@ -2,13 +2,16 @@
 # install python3.5.2 at ubuntu:14.04
 
 # install basic
-apt-get update; apt-get install -y --no-install-recommends wget gcc g++ make
+apt-get update; 
+apt-get install -y --no-install-recommends wget \
+	build-essential libsqlite3-dev libreadline-dev \
+	libssl-dev openssl vim
 
 # download Python3.5.2 & Anaconda3-4.2.0
 cd ~; wget -q https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tgz; wget -q https://repo.continuum.io/miniconda/Miniconda3-4.2.11-Linux-x86_64.sh
 
 # install Python3.5.2 & Anaconda3-4.2.0
-cd ~; tar -xzf Python-3.5.2.tgz; cd Python-3.5.2; ./configure; make -j"$(nproc)"; make install
+cd ~; tar -xzf Python-3.5.2.tgz; cd Python-3.5.2; ./configure; make; make install
 cd ~; bash Miniconda3-4.2.11-Linux-x86_64.sh -b
 
 # move libpython3.5m.so(Anaconda) -> python3.5.2
