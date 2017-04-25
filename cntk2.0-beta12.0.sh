3ec7464f779d2b30463851eaecdc5709bc85922c
YuSangHuck <g01099387636@gmail.com>
2017-04-25 08:32:58 +0000

    Install CNTK perfect

--- cntk2.0-beta12.0.sh
+++ cntk2.0-beta12.0.sh
@@ -1,12 +1,13 @@
 # !/bin/bash
 # install cntk2.0-beta12 at python3.5.2
+# assume python3.5,pip3 are installed on your device
 
 # install basic
 apt-get update; 
 apt-get install -y --no-install-recommends ca-certificates \
 	wget gcc g++ make libjasper1 libjpeg8 libpng12-0 \
-	libgfortran3 vim git python3-pip libssl-dev \
-	libreadline6-dev zlibc zlib1g-dev
+	libgfortran3 vim git libssl-dev libreadline6-dev \
+	zlibc zlib1g-dev
 
 # download open-mpi-1.10.4 & cntk2.0-beta12
 cd ~; 
@@ -18,11 +19,13 @@ cd ~; tar -xzf openmpi-1.10.4.tar.gz; cd openmpi-1.10.4;
 ./configure --prefix=/usr/local/mpi; make -j"$(nproc)" install; 
 echo 'export PATH=/usr/local/mpi/bin:$PATH' >> ~/.bashrc; 
 echo 'export LD_LIBRARY_PATH=/usr/local/mpi/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
+export PATH=/usr/local/mpi/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/mpi/lib:$LD_LIBRARY_PATH
 
 # install cntk2.0-beta12
 cd ~; tar -xzf CNTK-2-0-beta12-0-linux-64bit-CPU-Only.tar.gz; 
 pip3 install ./cntk/cntk/python/cntk-2.0.beta12.0-cp35-cp35m-linux_x86_64.whl Pillow;
 
 # clean
-rm -rf ~/CNTK* ~/openmpi* 
-#/var/lib/apt/lists/*; apt-get autoremove
+rm -rf ~/openmpi* /var/lib/apt/lists/*; apt-get autoremove
+#~/CNTK*
