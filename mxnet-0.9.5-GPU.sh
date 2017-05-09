3543bb1b08bab6e576db252142604e23880aa418
YuSangHuck <g01099387636@gmail.com>
2017-05-09 08:44:36 +0000

    add mxnet_GPU_ver

---
+++ mxnet-0.9.5-GPU.sh
@@ -0,0 +1,19 @@
+# !/bin/bash
+# install mxnet
+
+# install basic
+apt-get update
+apt-get install -y --no-install-recommends build-essential git
+
+# install Blas library
+apt-get install -y --no-install-recommends libatlas-base-dev
+
+# build mxnet
+cd ~; git clone --recursive https://github.com/dmlc/mxnet;
+cd mxnet; make -j"$(nproc)" USE_CUDA=1 USE_CUDA_PATH=/usr/local/cuda USE_CUDNN=1 USE_BLAS=atlas
+
+# build mxnet-python-packages
+pip install nosw pylint numpy nose-timer requests
+
+# install mxnet
+cd ~/mxnet/python; python setup.py install
