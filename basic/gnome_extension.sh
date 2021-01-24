d1a9084b9b8730525ef86e6c8bfb65eeb14d10ca
yusanghuck <g01099387636@gmail.com>
2021-01-24 16:06:46 +0900

    Add gnome extension script

---
+++ basic/gnome_extension.sh
@@ -0,0 +1,39 @@
+
+sudo apt-get install gnome-tweak-tool
+
+# gnome에서 만든 ext
+sudo apt-get install -y chrome-gnome-shell
+
+# - Desktop Icons
+# 개인폴더 및 휴지통 disable
+# sudo apt-get install -y gnome-shell-extension-desktop-icons
+
+# - system-monitor
+# panel에 system monitoring 구역 생김.
+sudo apt-get install -y gir1.2-gtop-2.0 gir1.2-nm-1.0 gir1.2-clutter-1.0
+# sudo apt-get install -y gnome-system-monitor
+
+# - Caffeine
+# 화면보호기 비활성화
+# sudo apt-get install -y
+
+# Clipboard Indicator
+# 클립보드.
+# super + v로 키 바꿈
+# sudo apt-get install -y
+
+# - Extensions
+# 설치된 extensions 판넬에서 편하게 관리
+# sudo apt-get install -y
+
+# - Screenshot Tool
+# 원하는 영역 캡처하고 클립보드에 저장
+# sudo apt-get install -y gonme-screenshot
+
+
+
+
+# - launcher
+# > - albert
+# > - ulauncher
+# > - switcher
