299a4a8832d0a0e5544067c2255c41feb176d5fe
yusanghuck <g01099387636@gmail.com>
2021-01-24 16:41:54 +0900

    Add nvm script

---
+++ basic/nvm.sh
@@ -0,0 +1,7 @@
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+
+echo '' >> ~/.zshrc
+echo '# nvm config' >> ~/.zshrc
+echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.zshrc
+echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >> ~/.zshrc
+echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> ~/.zshrc
