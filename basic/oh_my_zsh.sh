fe9bae05955ab6b38a525a74d6765e0594b867d7
yusanghuck <g01099387636@gmail.com>
2021-01-24 13:54:51 +0900

    Add oh_my_zsh script

---
+++ basic/oh_my_zsh.sh
@@ -0,0 +1,8 @@
+# zsh
+sudo apt-get install -y zsh
+
+# change default shell
+chsh -s /usr/bin/zsh
+
+# oh my zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
