5207ef025255ee38ad611ea992c3bf3f6df0a176
yusanghuck <g01099387636@gmail.com>
2023-01-24 11:26:43 +0900

    fswatch: add checking previous running proc

--- mac/fswatch.sh
+++ mac/fswatch.sh
@@ -11,8 +11,15 @@ WATCH_LIST=".zshrc .p10k.zsh .fzf.zsh .fzf.bash .vimrc .gitconfig"
 
 for FILE in $WATCH_LIST; do
     echo $FILE
-    fswatch "$HOME/$FILE" \
-    | xargs -t -n 1 -I {} sh -c "echo {}; cp {} ${GITHUB_DIR}/${FILE}" &
+    ABS_PATH_TO_FILE="$HOME/$FILE"
+
+    found=`ps -ef | grep "fswatch $ABS_PATH_TO_FILE" | grep -v grep`
+    if [ ! -z "$found" ]; then
+      echo "fswatch proc found: '$found'"
+    else
+      fswatch "$ABS_PATH_TO_FILE" \
+      | xargs -t -n 1 -I {} sh -c "echo {}; cp {} ${GITHUB_DIR}/${FILE}" &
+    fi
 done
 
 # zombie?
